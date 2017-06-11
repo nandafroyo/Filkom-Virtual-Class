e992ca52d16c5f7df2bbc9c4635fe96fe4839d1c
nandafroyo <failuretechno@yahoo.com>
2017-06-12 03:00:11 +0700

    Add files via upload

--- WebRTC-Scalable-Broadcast.js
+++ WebRTC-Scalable-Broadcast.js
@@ -1,16 +1,6 @@
 // Muaz Khan   - www.MuazKhan.com
 // MIT License - www.WebRTC-Experiment.com/licence
 
-// WebRTC Scalable Broadcast:
-// this module simply initializes socket.io
-// and configures it in a way that
-// single broadcast can be relayed over unlimited users
-// without any bandwidth/CPU usage issues.
-// Everything happens peer-to-peer!
-
-// Ref. discussion: https://github.com/muaz-khan/WebRTC-Experiment/issues/2
-// Source Code: https://github.com/muaz-khan/WebRTC-Scalable-Broadcast
-
 module.exports = exports = WebRTC_Scalable_Broadcast;
 
 function WebRTC_Scalable_Broadcast(app) {
@@ -18,21 +8,22 @@ function WebRTC_Scalable_Broadcast(app) {
         log: false,
         origins: '*:*'
     });
-
+/*
     io.set('transports', [
         'websocket', // 'disconnect' EVENT will work only with 'websocket'
         'xhr-polling',
         'jsonp-polling'
     ]);
-
+*/
     var listOfBroadcasts = {};
 
     io.on('connection', function(socket) {
         var currentUser;
-        socket.on('join-broadcast', function(user) {
+		
+        socket.on('join-broadcast', function(user, msg) {
             currentUser = user;
 
-            user.numberOfViewers = 0;
+            user.numberOfViewers =0;
             if (!listOfBroadcasts[user.broadcastid]) {
                 listOfBroadcasts[user.broadcastid] = {
                     broadcasters: {},
@@ -52,16 +43,49 @@ function WebRTC_Scalable_Broadcast(app) {
                 socket.emit('start-broadcasting', listOfBroadcasts[user.broadcastid].typeOfStreams);
 
                 console.log('User <', user.userid, '> will be next to serve broadcast.');
+					
             }
 
+			
             listOfBroadcasts[user.broadcastid].broadcasters[user.userid] = user;
             listOfBroadcasts[user.broadcastid].allusers[user.userid] = user;
+			//menampilkan nama user//
+			//console.log(listOfBroadcasts[user.broadcastid].allusers);
+			//console.log(listOfBroadcasts[user.broadcastid].broadcasters);
+			//console.log(listOfBroadcasts[user.broadcastid].broadcasters[user.userid]);
+			//console.log(currentUser.broadcastid);
+			
         });
 
-        socket.on('message', function(message) {
-            socket.broadcast.emit('message', message);
+        socket.on('message', function(message, userid) {
+            socket.broadcast.emit('message', message, userid);
         });
+		
+		/*
+		socket.on('chat message', function(msg){
+			//socket.join(broadcastid.broadcastid);
+			//socket.emit('chat message', msg);
+			//socket.to(broadcastid.broadcastid).emit('chat message', msg);
+			//socket.emit(broadcastid.broadcastid, msg);
+			//io.to(broadcastid.broadcastid).emit('chat message');
+			socket.emit('chat message', msg);
+			//socket.leave(broadcastid.broadcastid);
 
+			//console.log(broadcastid.broadcastid + msg);
+		});
+		*/
+		socket.on('socketCustomEvent', function(msg) {
+				
+				 socket.emit('socketCustomEvent', msg);
+			});
+			
+		socket.on('chat message', function(roomid, msg){
+			//console.log(roomid);
+			 socket.join(roomid);
+			io.sockets.in(roomid).emit('chat message', roomid, msg);
+			//console.log('end ' + roomid);
+		  });
+				 
         socket.on('disconnect', function() {
             if (!currentUser) return;
             if (!listOfBroadcasts[currentUser.broadcastid]) return;
@@ -72,17 +96,43 @@ function WebRTC_Scalable_Broadcast(app) {
                 delete listOfBroadcasts[currentUser.broadcastid];
             }
         });
+		
     });
+/*	
+var nsp = io.of(broadcastid);
+nsp.on('connection', function(socket){
+  console.log('someone connected');
+  	socket.on('chat message', function(msg, user){
+			//currentUser = user;
+			io.emit('chat message', msg);
+			//console.log(chatroom);
+		});
+});
+*/
+	
+	//socket.on(currentUser.broadcastid, function(msg, user){
+			
+			//io.emit(currentUser.broadcastid, msg);
+			//console.log(chatroom);
+		//});
+		
 
     function getFirstAvailableBraodcater(user) {
         var broadcasters = listOfBroadcasts[user.broadcastid].broadcasters;
+		//console.log(listOfBroadcasts[user.broadcastid].broadcasters);
+		//console.log(broadcasters);
         var firstResult;
         for (var userid in broadcasters) {
             if (broadcasters[userid].numberOfViewers <= 3) {
                 firstResult = broadcasters[userid];
-                continue;
-            } else delete listOfBroadcasts[user.broadcastid].broadcasters[userid];
+				
+				break;
+                //continue;
+            } //else delete listOfBroadcasts[user.broadcastid].broadcasters[userid];
         }
+		
+		console.log(firstResult);
         return firstResult;
+		
     }
 }
